73a523cd2d095c749544d1f9ea80ac36248f9e64
caioAugustoRodrigues <caio.turmaxii@gmail.com>
2020-07-26 23:29:18 -0300

    Updates

---
+++ precache-manifest.489dd2787b22245a0052e0b3945eff23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d244d47a4d862ee9f834d00c32fb4cd7",
+    "url": "/Local-Weather/index.html"
+  },
+  {
+    "revision": "816ffb7437ace553fcd0",
+    "url": "/Local-Weather/static/css/main.80e7c876.chunk.css"
+  },
+  {
+    "revision": "d172c9924516b33e7d4a",
+    "url": "/Local-Weather/static/js/2.2c36bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Local-Weather/static/js/2.2c36bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816ffb7437ace553fcd0",
+    "url": "/Local-Weather/static/js/main.0e5c3b62.chunk.js"
+  },
+  {
+    "revision": "2905ae5e1060090ca031",
+    "url": "/Local-Weather/static/js/runtime-main.93ffe25f.js"
+  }
+]);
